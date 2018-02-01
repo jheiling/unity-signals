0214c9901d331fcced659012b1c95d2fe1ea381b
Jürgen Heiling <juergen.heiling@gmail.com>
2018-02-01 22:08:23 +0100

    improved example

---
+++ Example/Code/PositionSignalSetter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Signals.Common;
+
+
+
+namespace Signals.Example
+{
+    [AddComponentMenu("Signals/Example/PositionSignalSetter")]
+    public class PositionSignalSetter : MonoBehaviour
+    {
+        public Vector3Signal Signal;
+        Transform _transform;
+
+        void Awake()
+        {
+            _transform = transform;
+        }
+
+        void Update()
+        {
+            Signal.Value = _transform.position;
+        }
+    }
+}
