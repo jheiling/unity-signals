3f1477cb4312cf695c4e6ff5a30685170b28dd59
Jürgen Heiling <juergen.heiling@gmail.com>
2018-02-12 14:55:13 +0100

    moved ISignal into separate file

---
+++ Code/ISignal.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.Events;
+
+
+
+namespace Signals
+{
+    /// <summary>
+    /// The Signal interface.
+    /// </summary>
+    /// <typeparam name="T">The type of the <see cref="Value"/>.</typeparam>
+    /// <typeparam name="ET">The type of the <see cref="OnChanged"/> event.</typeparam>
+    public interface ISignal<T, ET> where ET : UnityEvent<T>, new()
+    {
+        /// <summary>
+        /// The current value of the Signal. 
+        /// Setting the value triggers the <see cref="OnChanged"/> event. 
+        /// </summary>
+        T Value { get; set; }
+
+        /// <summary>
+        /// The event invoked when a <see cref="Value"/> is assigned to the Signal.
+        /// </summary>
+        ET OnChanged { get; }
+    }
+}
