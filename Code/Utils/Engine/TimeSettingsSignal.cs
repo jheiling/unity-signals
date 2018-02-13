dc83c0624a25676a0d57ec1a1ff1c7e738256f20
Jürgen Heiling <juergen.heiling@gmail.com>
2018-02-13 11:40:20 +0100

    fixed menu

--- Code/Utils/Engine/TimeSettingsSignal.cs
+++ Code/Utils/Engine/TimeSettingsSignal.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 namespace Signals.Utils.Engine
 {
-    [CreateAssetMenu(menuName = "Signals/TimeSettingsSignal")]
+    [CreateAssetMenu(menuName = "Signals/Utils/Engine/TimeSettingsSignal")]
     public class TimeSettingsSignal : Signal<TimeSettings, TimeSettingsEvent> { }
 }
